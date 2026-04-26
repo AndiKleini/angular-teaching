d9ab7a3e27d9237b7221847850cba1b68664b011
AndiKleini <Andi.Kleinbichler@gmail.com>
2026-04-26 10:32:22 +0200

    feature: Protect endpoints with JWT token.

--- api/src/main/java/at/smarthome/config/SecurityConfig.java
+++ api/src/main/java/at/smarthome/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package at.smarthome.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,21 +8,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import at.smarthome.filter.JwtAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private JwtAuthenticationFilter jwtAuthFilter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
         http
-        .csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .formLogin().disable()
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and())
+        .formLogin(form -> form.disable())
         .securityMatcher("/**")
-        .authorizeHttpRequests(registry -> registry
+        .authorizeHttpRequests(auth -> auth
             .requestMatchers("/auth").permitAll()
             .anyRequest().authenticated()
-        );
+        )
+        .addFilterBefore(jwtAuthFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
---
+++ api/src/main/java/at/smarthome/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package at.smarthome.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.smarthome.service.AuthenticationService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    
+    private final AuthenticationService authService;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        this.log.info("JwtAuthenticationFilter: doFilterInternal called for request " + request.getRequestURI());
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        // 1. Check for Bearer token
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            this.log.info("JwtAuthenticationFilter: No Bearer token found, skipping authentication");
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        if (this.authService.validate(jwt)) {
+            this.log.info("JwtAuthenticationFilter: Invalid JWT token");
+             UsernamePasswordAuthenticationToken authToken = 
+                new UsernamePasswordAuthenticationToken(
+                        124, null, List.of(() -> "ROLE_USER"));
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        };
+      
+        this.log.info("JwtAuthenticationFilter: Valid JWT token found, setting authentication context");
+        filterChain.doFilter(request, response);
+    }
+}
